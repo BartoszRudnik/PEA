6a43f9d41e77e7ab871db4e6a21b568f9bd81e08
Bartosz Rudnik <rudnik49@gmail.com>
2020-10-17 18:28:47 +0200

    da

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,6 @@ public class Main {
 
     public static void main(String[] args){
 
-        FullSearch fs = new FullSearch();
-
-        int graph[][] = { { 0, 10, 15, 20 },
-                        { 10, 0, 35, 25 },
-                        { 15, 35, 0, 30 },
-                        { 20, 25, 30, 0 } };
-
-        fs.setV(4);
-
-        System.out.print(fs.algorithm(graph));
 
 
     }
